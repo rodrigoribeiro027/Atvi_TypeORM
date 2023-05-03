736c1bc2b72393d6d16f32539b04c2740421fd7e
rodrigoribeiro027 <rodrigo.rsantos40@gmail.com>
2023-05-03 20:25:35 -0300

    Finalizado crud Matches

--- src/controllers/MatchesController.ts
+++ src/controllers/MatchesController.ts
@@ -52,6 +52,40 @@ class MatchesController {
             res.status(500).json(error);
         }
     }
+    public async deleteMatch(req: Request, res: Response) {
+        try {
+            const { id } = req.body;
+            const data = await database.createQueryBuilder().delete().from(Matches).where("id=:id", { id }).execute();
+            return res.json(data);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+    public async updateMatch(req: Request, res: Response) {
+        try {
+            const { id, idhost, idvisitor, date } = req.body;
+
+            const host = await database.getRepository(Teams).findOne({ where: { id: idhost } });
+            if (!host) {
+                return res.status(400).json({ error: "Mandante desconhecido" });
+            }
+            const visitor = await database.getRepository(Teams).findOne({ where: { id: idvisitor } });
+            if (!visitor) {
+                return res.status(400).json({ error: "Visitante desconhecido" });
+            }
+            const match = await database.getRepository(Matches).findOne({ where: { id } });
+            if (!match) {
+                return res.status(400).json({ error: "Jogo não encontrado" });
+            }
+            match.date = date;
+            match.host = host;
+            match.visitor = visitor;
+            await database.getRepository(Matches).save(match);
+            return res.json(match);
+        } catch (error) {
+            return res.status(500).json({ error: "Erro ao atualizar jogo" });
+        }
+    }
 }
 
 export default new MatchesController();
